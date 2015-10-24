d35985a82ef802c3d830cfd2400aad332e94a250
Jeong, Jiung <ethernuiel@sanultari.com>
2015-10-25 02:15:13 +0900

    fix railtie names

--- lib/rails/semantic_ui/railtie.rb
+++ lib/rails/semantic_ui/railtie.rb
@@ -1,12 +1,16 @@
 require 'rails/webpack'
 require 'rails'
 
-module Rails::Webpack
+module Rails::SemanticUI
   class Railtie < Rails::Railtie
     railtie_name :'rails-semantic-ui'
 
     rake_tasks do
       load 'tasks/semantic_ui.rake'
+      #load 'tasks/gulp.rake'
+      #load 'tasks/npm.rake'
+      #load 'tasks/prepare.rake'
+      #load 'tasks/webpack.rake'
     end
   end
 end
