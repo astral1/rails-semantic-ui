4c90aa7e38c7452aa5420edc4b7c7e0945f26309
Jeong, Jiung <ethernuiel@sanultari.com>
2015-10-25 02:37:22 +0900

    add railtie for semantic_ui

--- lib/rails/semantic_ui.rb
+++ lib/rails/semantic_ui.rb
@@ -1,6 +1,7 @@
 module Rails
   module SemanticUI
     require 'rails/semantic_ui/version'
+    require 'rails/semantic_ui/railtie'
 
     autoload :Dependencies, 'rails/semantic_ui/dependencies'
   end
